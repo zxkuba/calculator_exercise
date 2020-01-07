3a6f109f2ad539b53c9fb20978c6fb40436d9cb6
zxkuba <jakub.krzeszowiak@gmail.com>
2020-01-07 20:51:33 +0100

    calc

---
+++ src/main/java/com/calculator/exercise/Calculator.java
@@ -0,0 +1,22 @@
+package com.calculator.exercise;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        
+    }
+
+    public int add(int a, int b){
+        int sum;
+        sum = a+b;
+        return sum;
+    }
+
+    public int sub(int a, int b){
+        int sub;
+        sub = a - b;
+        return sub;
+    }
+
+}
